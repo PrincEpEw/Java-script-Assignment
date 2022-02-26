95b9dd6d191402ca5ea9fc1a52e430b460cdd785
PrincEpEw <reyazulislamprince@gmail.com>
2022-02-26 22:55:21 +0600

    assignment js 1

---
+++ Assignment 1/devs.js
@@ -0,0 +1,32 @@
+/*Make a devs info console project*/
+// var name="prince"
+// var age= 20
+// var company_name="software ltd"
+// var Home_town="Dhaka"
+// console.log(name,age,company_name,Home_town);
+
+
+/*Make a result marks project*/
+var math=prompt("put your math marks:")
+var bangla=prompt("put your bangla marks:")
+var english=prompt("put your english marks:")
+var physics=prompt("put your physics marks:")
+var biology=prompt("put your biology marks:")
+var chemistry=prompt("put your chemistry marks:")
+
+alert   ("math mark:"+  math + " "+ "bangla mark :  "+ bangla+ " "+ "english mark :   " + english+ " "+ "physics mark:   "+ physics + " "+"biology mark :  "+ biology + " "+ "chemistry mark:  " + " "+chemistry );
+
+
+
+/*make a kobol project*/
+
+var boy_name = prompt('Enter Your Name:');
+confirm("chele Apni ki kobul bolte raji");
+var girl_name=prompt("Enter your name");
+confirm("meye apni ki kobul bolte raji")
+
+
+
+
+
+
